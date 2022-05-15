d648aac9d0dccd330c2b52a4dbf38d7fa6c5d18d
odoyonina <odoyoninah@gmail.com>
2022-05-16 00:58:49 +0300

    add blogs

--- PROJECTS/BlogUp/app/main/views.py
+++ PROJECTS/BlogUp/app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_required
+from flask_login import login_required
 
 from app.main.forms import PostForm
 from app.models import Post
