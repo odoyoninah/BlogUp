273b25bd41b9d51eeda18b2a00e7d189dc7911ce
odoyonina <odoyoninah@gmail.com>
2022-05-15 21:16:54 +0300

    create manager shell

---
+++ PROJECTS/BlogUp/manage.py
@@ -0,0 +1,18 @@
+from app import create_app,db
+from flask_script import Manager, Server
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User )
+
+if __name__ == '__main__':
+    manager.run()
